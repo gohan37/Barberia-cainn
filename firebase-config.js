289c7a90df1896846832ba5ef607e497fbdda254
gohan37 <nachofernandez8567@gmail.com>
2025-04-11 00:10:02 -0300

    Agrego firebase config para testeo en GitHub Pages

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBpXLjRauFW1t7gv-iR9LQUfc5fs9Siv3k",
+    authDomain: "barberia-494e1.firebaseapp.com",
+    projectId: "barberia-494e1",
+    storageBucket: "barberia-494e1.appspot.com",
+    messagingSenderId: "493660833166",
+    appId: "1:493660833166:web:6e5949deaf6cb71eb710df"
+  };
